4df0fd22998054c25244b080b7552abf7492edc1
Ralenze <53167790+Ralenze@users.noreply.github.com>
2022-10-23 23:30:53 -0700

    Add files via upload
    Got first portion done... honestly just following thru a youtube tutorial on websites but imma customize it later on so that its more authentic

---
+++ index.js
@@ -0,0 +1,15 @@
+function showBtn(){
+    console.log("made it here");
+   let menubutton =  document.getElementById('menubtn');
+   let menu = document.getElementById('menu');
+   console.log(menu);
+   menu.style.display = "block";
+}
+
+function hideBtn(){
+    console.log("made it here");
+   let menubutton =  document.getElementById('hidebtn');
+   let menu = document.getElementById('menu');
+   console.log(menu);
+   menu.style.display = "none";
+}
