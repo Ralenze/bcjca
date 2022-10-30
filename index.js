61c469493fb9d7a425d08c03882e3bbac4bee268
belw7898 <77554409+belw7898@users.noreply.github.com>
2022-10-30 10:09:15 -0700

    calendar update

--- index.js
+++ index.js
@@ -14,84 +14,57 @@ function hideBtn(){
    menu.style.display = "none";
 }
 
-var eventDates = {}
-let day1 = formatDate(new Date(new Date().setMonth(new Date().getMonth() + 1)))
-eventDates[day1] = [
-  'BCJCA Championship (tentative), BCJCA Headquarters',
-]
-let day2 = formatDate(new Date(new Date().setDate(new Date().getDate() + 40)))
-eventDates[day2] = [
-  'Event 2, Location 3',
-]
+let cardElement = document.querySelector(".calcard");
 
-// set maxDates
-var maxDate = {
-  1: new Date(new Date().setMonth(new Date().getMonth() + 11)),
-  2: new Date(new Date().setMonth(new Date().getMonth() + 10)),
-  3: new Date(new Date().setMonth(new Date().getMonth() + 9))
+cardElement.addEventListener("click", flip);
+
+function flip(){
+  cardElement.classList.toggle("flipped")
 }
 
-var flatpickr = $('#calendar .placeholder').flatpickr({
-  inline: true,
-  minDate: 'today',
-  maxDate: maxDate[3]
-,
-  showMonths: 1,
-  enable: Object.keys(eventDates),
-  disableMobile: "true",
-  onChange: function(date, str, inst) {
-    var contents = '';
-    if(date.length) {
-        for(i=0; i < eventDates[str].length; i++) {
-        contents += '<div class="event"><div class="date">' + flatpickr.formatDate(date[0], 'l J F') + '</div><div class="location">' + eventDates[str][i] + '</div></div>';
-      }
-    }
-    $('#calendar .calendar-events').html(contents)
-  },
-  locale: {
-    weekdays: {
-      shorthand: ["S", "M", "T", "W", "T", "F", "S"],
-      longhand: [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ]
-    }
-  }
-})
-
-eventCaledarResize($(window));
-$(window).on('resize', function() {
-  eventCaledarResize($(this))
-})
-
-function eventCaledarResize($el) {
-  var width = $el.width()
-  if(flatpickr.selectedDates.length) {
-    flatpickr.clear()
-  }
-  if(width >= 992 && flatpickr.config.showMonths !== 3) {
-    flatpickr.set('showMonths', 3)
-    flatpickr.set('maxDate', maxDate[3])
-  }
-  if(width < 992 && width >= 768 && flatpickr.config.showMonths !== 2) {
-    flatpickr.set('showMonths', 2)
-    flatpickr.set('maxDate', maxDate[2])
-  }
-  if(width < 768 && flatpickr.config.showMonths !== 1) {
-    flatpickr.set('showMonths', 1)
-    flatpickr.set('maxDate', maxDate[1])
-    $('.flatpickr-calendar').css('width', '')
-  }
+function startTime() {
+    var weekday = new Array();
+    weekday[0] =  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var d = today.getDate();
+    var y = today.getFullYear();
+    var wd = weekday[today.getDay()];
+    var mt = month[today.getMonth()];
+  
+    m = checkTime(m);
+    s = checkTime(s);
+                    document.getElementById('date').innerHTML =
+    d;
+ document.getElementById('day').innerHTML =
+    wd;
+ document.getElementById('month').innerHTML =
+    mt + "/" + y;
+    
+    var t = setTimeout(startTime, 500);
 }
-
-function formatDate(date) {
-    let d = date.getDate();
-    let m = date.getMonth() + 1; //Month from 0 to 11
-    let y = date.getFullYear();
-    return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
 }
