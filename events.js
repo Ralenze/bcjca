fda66aea90354efea7e74c6a72a040fc4bee9773
belw7898 <77554409+belw7898@users.noreply.github.com>
2022-11-01 20:45:05 -0700

    update events

---
+++ events.js
@@ -0,0 +1,57 @@
+let cardElement = document.querySelector(".card");
+let editButton = document.querySelector(".edit");
+let saveButton = document.querySelector(".save");
+
+editButton.addEventListener("click", flip);
+saveButton.addEventListener("click", flip);
+
+function flip(){
+  cardElement.classList.toggle("flipped")
+}
+
+function startTime() {
+    var weekday = new Array();
+    weekday[0] =  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var month = new Array();
+    month[0] = "Jan";
+    month[1] = "Feb";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "Aug";
+    month[8] = "Sept";
+    month[9] = "Oct";
+    month[10] = "Nov";
+    month[11] = "Dec";
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var d = today.getDate();
+    var y = today.getFullYear();
+    var wd = weekday[today.getDay()];
+    var mt = month[today.getMonth()];
+  
+    m = checkTime(m);
+    s = checkTime(s);
+                    document.getElementById('date').innerHTML =
+    d;
+ document.getElementById('day').innerHTML =
+    wd;
+ document.getElementById('month').innerHTML =
+    mt + " " + y;
+    
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
