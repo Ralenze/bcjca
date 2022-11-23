dc0a37fcc4c20686387de5e9a0395e904e3f971b
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2022-11-22 18:43:08 -0800

    Basic code cleanup for readability

--- index.js
+++ index.js
@@ -3,40 +3,35 @@ script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 // ... give time for script to load, then type (or see below for non wait option)
 
-function showBtn(){
+function showBtn() {
     console.log("made it here");
-   let menubutton =  document.getElementById('menubtn');
-   let menu = document.getElementById('menu');
-   console.log(menu);
-   menu.style.display = "block";
-   menubutton.style.display = "none";
+    let menubutton =  document.getElementById('menubtn');
+    let menu = document.getElementById('menu');
+    console.log(menu);
+    menu.style.display = "block";
+    menubutton.style.display = "none";
 }
 
-function hideBtn(){
+function hideBtn() {
     console.log("made it here");
-   let menubutton =  document.getElementById('hidebtn');
-   let menu = document.getElementById('menu');
-   console.log(menu);
-   menu.style.display = "none";
-   let x =  document.getElementById('menubtn');
-   x.style.display = "block";
-
-
+    let menubutton =  document.getElementById('hidebtn');
+    let menu = document.getElementById('menu');
+    console.log(menu);
+    menu.style.display = "none";
+    let x =  document.getElementById('menubtn');
+    x.style.display = "block";
 }
+
 function openExternalLink(a){
     console.log("made it here");
     switch(a){
         case 1:
-
             window.open('https://github.com/Ralenze/bcjca', "_blank");
             break;
         case 2:
             window.open('https://www.instagram.com/bcjrchess', "_blank");
-            break;
-    
-            
+            break; 
     }
-
 }
 let b = window.location.pathname;
 console.log(b);
@@ -44,21 +39,15 @@ let x = b.toString();
 
 function getPromiseFromEvent(item, event) {
     return new Promise( (resolve) => {
-      const listener = async () => {
-        
-        item.removeEventListener(event, listener);
-        item.classList.add('hide')
-        resolve();
-        return;
-       
-
-       
-      }
-      item.classList.add("aboutslider");
-      item.classList.remove("hide")
-      item.addEventListener(event, listener)
-    
-      
+        const listener = async () => {
+            item.removeEventListener(event, listener);
+            item.classList.add('hide');
+            resolve();
+            return;
+        }
+        item.classList.add("aboutslider");
+        item.classList.remove("hide");
+        item.addEventListener(event, listener);
     })
   }
 
@@ -66,73 +55,69 @@ function getPromiseFromEvent(item, event) {
 This function selects all the span/words under our club title (BCJCA is about ). Then it selects one
 to slide in and make visible. Repeats using incrementer
   */
- const slidetextabout = async () => {
+const slidetextabout = async () => {
     let slidex = document.getElementsByClassName('clubtitle');
    
     let slider = slidex[0].children;
     console.log(slider);
     let incrementer = 0;
-    while(true){
+    while(true) {
         let moduloselector = incrementer % 4;
         console.log(slider[moduloselector]);
 
-await getPromiseFromEvent(slider[moduloselector], "webkitAnimationEnd");
+        await getPromiseFromEvent(slider[moduloselector], "webkitAnimationEnd");
         incrementer++;
         console.log("iter finish");
-
     }
- }
+}
 
 
  /* 
  Basically, if we are in the about page we want some words to go in and out and we want to have 
  the orange bar underneath about... TODO that.
  */
- if (x.includes("about")){
-   
-     slidetextabout();
-     
+if (x.includes("about")) {
+    slidetextabout();
 }
 
-$(document).ready(function(){
-
-const calling = async() => {
-    console.log("Made it!");
-    console.log($(window).scrollTop());
-
-  setTimeout(1000, calling);
-  
-}
-
-
-let displayCardRows = [true, true, true, true, true, true, true];
-let incrementer = 0;
-let b = window.location.pathname;
-console.log(b);
-let x = b.toString();
-const loadCards = async() => {if(($(window).scrollTop() + $(window).height() >= (1050 + 700 * incrementer) && x.includes("about") &&  displayCardRows[incrementer] == true)) {
-
-  $('.clubcardrow')[incrementer].style.opacity = 1;
-  $('.clubcardrow')[incrementer].style.left = 0;
-  incrementer++;
-  loadCards();
-  console.log(incrementer);
-}}
-loadCards();
-$(window).scroll(loadCards); 
-} 
-);
+$(document).ready(function() {
+    const calling = async() => {
+        console.log("Made it!");
+        console.log($(window).scrollTop());
+        setTimeout(1000, calling); 
+    }
+    let displayCardRows = [true, true, true, true, true, true, true];
+    let incrementer = 0;
+    let b = window.location.pathname;
+    console.log(b);
+    let x = b.toString();
+    const loadCards = async() => {
+        if (
+            $(window).scrollTop() + $(window).height() >= (1050 + 700 * incrementer) 
+            && x.includes("about") 
+            && displayCardRows[incrementer] == true
+        ) {
+            $('.clubcardrow')[incrementer].style.opacity = 1;
+            $('.clubcardrow')[incrementer].style.left = 0;
+            incrementer++;
+            loadCards();
+            console.log(incrementer);
+        }
+    }
+    loadCards();
+    $(window).scroll(loadCards); 
+});
 document.getElementById('light').style.color = 'white';
 
 function letThereBeLight(){
-  let icon = document.getElementById('light');
-  let logo = document.getElementById('logo');
-  if(icon.style.color === "white"){
-    icon.style.color = "rgba(255, 240, 0, 1)";
-    logo.src = "https://github.com/Ralenze/bcjca/blob/main/images/bcjcalight.png?raw=true"
-  }else{
-    icon.style.color = "white";
-    logo.src = "https://github.com/Ralenze/bcjca/blob/main/images/bcjcalogo.png?raw=true"
-  }
+    let icon = document.getElementById('light');
+    let logo = document.getElementById('logo');
+    if (icon.style.color === "white") {
+        icon.style.color = "rgba(255, 240, 0, 1)";
+        logo.src = "https://github.com/Ralenze/bcjca/blob/main/images/bcjcalight.png?raw=true"
+    } else {
+        icon.style.color = "white";
+        logo.src = "https://github.com/Ralenze/bcjca/blob/main/images/bcjcalogo.png?raw=true"
+    }
 }
 
