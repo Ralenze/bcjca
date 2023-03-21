8c38b181875b32c3026cecf84267f68390158a18
Luke Nguyen Khanh <64694123+LukeNk@users.noreply.github.com>
2023-03-21 22:02:48 +0000

    Clean up codes and putting files into directories

--- scripts/events.js
+++ scripts/events.js
@@ -1,7 +1,3 @@
-var script = document.createElement("script");
-script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
-document.getElementsByTagName("head")[0].appendChild(script);
-
 $(document).ready(function () {
     let toSlide = $(".slider");
     toSlide[0].style.opacity = "1";
