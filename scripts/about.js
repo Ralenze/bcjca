11cac43edeaca29a3fbb6af4f65a5649d642a779
Toricane <73972068+Toricane@users.noreply.github.com>
2023-06-09 22:35:34 -0700

    feat!: new clubs design in about page

--- scripts/about.js
+++ scripts/about.js
@@ -1,166 +1,3 @@
-/*  The below code is for the legacy club cards that were
-    in place before the conveyor belt was implemented.     */
-
-// function schoolCards() {
-//     console.log("schoolcards");
-//     fetch("info/clubinfo.json")
-//         .then((response) => response.json())
-//         .then((data) => {
-//             data.shift();
-//             const clubCardHolder = document.getElementById("clubcardholder");
-//             clubCardHolder.innerHTML = "";
-//             clubCardHolder.className = "grid-container animate-grid";
-//             let clubCard,
-//                 info,
-//                 h2,
-//                 contact,
-//                 p,
-//                 leaders,
-//                 text,
-//                 last,
-//                 a,
-//                 clubCardTop,
-//                 img,
-//                 clubLeader,
-//                 clubCardBottom;
-
-//             for (let i = 0; i < data.length; i++) {
-//                 clubCard = document.createElement("div");
-//                 clubCard.className = "clubcard grid-item";
-
-//                 info = document.createElement("div");
-//                 info.className = "info";
-
-//                 h2 = document.createElement("h2");
-//                 h2.innerHTML = `-EST. ${data[i].est}-`;
-//                 h2.style = "padding-bottom: 0;";
-//                 info.appendChild(h2);
-
-//                 contact = data[i]["contact"];
-
-//                 if (contact.leaders.length > 0) {
-//                     p = document.createElement("p");
-//                     leaders = contact.leaders;
-//                     text = "";
-//                     if (leaders.length === 1) {
-//                         text = leaders[0];
-//                     } else if (leaders.length === 2) {
-//                         text = leaders.join(" and ");
-//                     } else if (leaders.length > 2) {
-//                         last = leaders.pop();
-//                         text = `${leaders.join(", ")}, and ${last}`;
-//                     }
-//                     p.innerHTML = `<i class="fa-solid fa-users"></i> ${text}`;
-//                     info.appendChild(p);
-//                 }
-
-//                 if (contact.email) {
-//                     p = document.createElement("p");
-//                     a = document.createElement("a");
-//                     a.href = `mailto:${contact.email}`;
-//                     a.innerHTML = `<i class="fa-solid fa-envelope"></i> Contact Us`;
-//                     a.target = "_blank";
-//                     a.rel = "noopener noreferrer";
-//                     p.appendChild(a);
-//                     info.appendChild(p);
-//                 }
-
-//                 if (contact.location) {
-//                     p = document.createElement("p");
-//                     p.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${contact.location}`;
-//                     info.appendChild(p);
-//                 }
-
-//                 if (contact.discord) {
-//                     p = document.createElement("p");
-//                     a = document.createElement("a");
-//                     a.href = contact.discord;
-//                     a.innerHTML = `<i class="fa-brands fa-discord"></i> Discord`;
-//                     a.target = "_blank";
-//                     a.rel = "noopener noreferrer";
-//                     p.appendChild(a);
-//                     info.appendChild(p);
-//                 }
-
-//                 if (contact.instagram) {
-//                     p = document.createElement("p");
-//                     a = document.createElement("a");
-//                     a.href = `https://instagram.com/${contact.instagram}`;
-//                     a.innerHTML = `<i class="fa-brands fa-instagram"></i> Instagram`;
-//                     a.target = "_blank";
-//                     a.rel = "noopener noreferrer";
-//                     p.appendChild(a);
-//                     info.appendChild(p);
-//                 }
-
-//                 clubCard.appendChild(info);
-
-//                 clubCardTop = document.createElement("div");
-//                 clubCardTop.className = "clubcardtop";
-//                 img = document.createElement("img");
-//                 if (data[i].image === "") {
-//                     img.src = "images/defaultclublogo.jpg";
-//                 } else {
-//                     img.src = data[i].image;
-//                 }
-//                 img.alt = data[i].name;
-//                 img.width = "200";
-//                 img.height = "200";
-//                 clubCardTop.appendChild(img);
-
-//                 clubLeader = document.createElement("div");
-//                 clubLeader.className = "clubleader";
-//                 clubCardTop.appendChild(clubLeader);
-//                 clubCard.appendChild(clubCardTop);
-
-//                 clubCardBottom = document.createElement("div");
-//                 clubCardBottom.className = "clubcardbottom";
-
-//                 h2 = document.createElement("h2");
-//                 h2.innerHTML = data[i].name;
-//                 clubCardBottom.appendChild(h2);
-//                 clubCard.appendChild(clubCardBottom);
-
-//                 clubCardHolder.appendChild(clubCard);
-//             }
-//         })
-//         .catch((error) => console.log(error));
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     window.addEventListener("scroll", function () {
-//         const grid = document.querySelector(".animate-grid");
-//         const gridItems = document.querySelectorAll(".grid-item");
-//         const viewportHeight = window.innerHeight;
-//         const gridRect = grid.getBoundingClientRect();
-//         const gridTop = gridRect.top;
-//         const gridBottom = gridRect.bottom;
-
-//         if (gridTop < viewportHeight && gridBottom > 0) {
-//             grid.style.opacity = 1;
-//             gridItems.forEach((item) => {
-//                 const itemRect = item.getBoundingClientRect();
-//                 const itemTop = itemRect.top;
-//                 const itemBottom = itemRect.bottom;
-
-//                 if (itemTop < viewportHeight && itemBottom > 0) {
-//                     item.style.transform = "translateY(0)";
-//                     item.style.opacity = 1;
-//                 } else {
-//                     item.style.transform = "translateY(100px)";
-//                     item.style.opacity = 0;
-//                 }
-//             });
-//         } else {
-//             grid.style.opacity = 0;
-//             gridItems.forEach((item) => {
-//                 item.style.transform = "translateY(100px)";
-//                 item.style.opacity = 0;
-//             });
-//         }
-//     });
-// });
-
 function getPromiseFromEvent(item, event) {
     return new Promise((resolve) => {
         const listener = async () => {
@@ -225,12 +62,16 @@ fetch("info/clubinfo.json")
                 const img1 = document.createElement("img");
                 img1.src = club1.image;
                 img1.alt = club1.name;
+                img1.onclick = openPopup;
+                img1.onmouseover = openPopup;
                 photobanner1.appendChild(img1);
 
                 const club2 = shuffledData2[i];
                 const img2 = document.createElement("img");
                 img2.src = club2.image;
                 img2.alt = club2.name;
+                img2.onclick = openPopup;
+                img2.onmouseover = openPopup;
                 photobanner2.appendChild(img2);
             }
         }
@@ -242,7 +83,7 @@ function updatePhotoDisplay() {
 
     maxScreenLength = Math.max(screenWidth, screenHeight);
     numPhotos = Math.floor(maxScreenLength / (100 + 8));
-    numLoops = 1 + Math.floor(numPhotos / Math.floor(storedData.length / 2));
+    numLoops = 2 + Math.floor(numPhotos / Math.floor(storedData.length / 2));
 
     // Clear existing photos
     photobanner1.innerHTML = "";
@@ -255,15 +96,132 @@ function updatePhotoDisplay() {
             const img1 = document.createElement("img");
             img1.src = club1.image;
             img1.alt = club1.name;
+            img1.onclick = openPopup;
+            img1.onmouseover = openPopup;
             photobanner1.appendChild(img1);
 
             const club2 = shuffledData2[i];
             const img2 = document.createElement("img");
             img2.src = club2.image;
             img2.alt = club2.name;
+            img2.onclick = openPopup;
+            img2.onmouseover = openPopup;
             photobanner2.appendChild(img2);
         }
     }
 }
 
 window.addEventListener("resize", updatePhotoDisplay);
+
+function resetPopup(event) {
+    const popup = document.getElementById("popup");
+    console.log(event.target);
+    console.log(
+        popup.open,
+        event.target !== popup,
+        !popup.contains(event.target)
+    );
+    if (
+        popup.open &&
+        event.target !== popup &&
+        !popup.contains(event.target) &&
+        event.target.parentNode.className !== "photobanner" &&
+        event.target.className !== "photobanner"
+    ) {
+        popup.close();
+        popup.open = false;
+        event.preventDefault();
+    }
+}
+
+document.addEventListener("click", resetPopup);
+document.addEventListener("mouseover", resetPopup);
+
+function openPopup(event) {
+    const popup = document.getElementById("popup");
+    if (popup.open) {
+        popup.open = false;
+    }
+
+    fetch("info/clubinfo.json")
+        .then((response) => response.json())
+        .then((data) => {
+            data.shift();
+            const club = data.find((club) => club.name === event.target.alt);
+            const popupContent = document.getElementById("popup-content");
+            popupContent.innerHTML = "";
+
+            let h2 = document.createElement("h2");
+            h2.innerHTML = club.name;
+            popupContent.appendChild(h2);
+
+            const contact = club["contact"];
+
+            if (contact.leaders.length > 0) {
+                p = document.createElement("p");
+                leaders = contact.leaders;
+                text = "";
+                if (leaders.length === 1) {
+                    text = leaders[0];
+                } else if (leaders.length === 2) {
+                    text = leaders.join(" and ");
+                } else if (leaders.length > 2) {
+                    last = leaders.pop();
+                    text = `${leaders.join(", ")}, and ${last}`;
+                }
+                p.innerHTML = `<i class="fa-solid fa-users"></i> ${text}`;
+                popupContent.appendChild(p);
+            }
+
+            if (contact.email) {
+                p = document.createElement("p");
+                a = document.createElement("a");
+                a.href = `mailto:${contact.email}`;
+                a.innerHTML = `<i class="fa-solid fa-envelope"></i> Contact Us`;
+                a.target = "_blank";
+                a.rel = "noopener noreferrer";
+                p.appendChild(a);
+                popupContent.appendChild(p);
+            }
+
+            if (contact.location) {
+                p = document.createElement("p");
+                p.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${contact.location}`;
+                popupContent.appendChild(p);
+            }
+
+            if (contact.discord) {
+                p = document.createElement("p");
+                a = document.createElement("a");
+                a.href = contact.discord;
+                a.innerHTML = `<i class="fa-brands fa-discord"></i> Discord`;
+                a.target = "_blank";
+                a.rel = "noopener noreferrer";
+                p.appendChild(a);
+                popupContent.appendChild(p);
+            }
+
+            if (contact.instagram) {
+                p = document.createElement("p");
+                a = document.createElement("a");
+                a.href = `https://instagram.com/${contact.instagram}`;
+                a.innerHTML = `<i class="fa-brands fa-instagram"></i> Instagram`;
+                a.target = "_blank";
+                a.rel = "noopener noreferrer";
+                p.appendChild(a);
+                popupContent.appendChild(p);
+            }
+
+            clubCardTop = document.createElement("div");
+            clubCardTop.className = "clubcardtop";
+            const popupImage = document.getElementById("popup-image");
+            if (club.image === "") {
+                popupImage.src = "images/defaultclublogo.jpg";
+            } else {
+                popupImage.src = club.image;
+            }
+            img.alt = club.name;
+        });
+
+    popup.show();
+}
